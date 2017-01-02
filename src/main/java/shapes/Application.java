3fbd92ace0dc78a19f78f949b538e9612693d4a2
Thomas TH <tth@e-conomic.com>
2017-01-02 19:31:38 +0100

    Hello world implemented

---
+++ src/main/java/shapes/Application.java
@@ -0,0 +1,15 @@
+package shapes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author tth
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
--- src/main/java/shapes/TriangleController.java
+++ src/main/java/shapes/TriangleController.java
@@ -1,9 +1,21 @@
 package shapes;
 
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author tth
  */
+@RestController
 public class TriangleController {
+    private static final String template = "Hellp, %s!";
+    private final AtomicLong counter = new AtomicLong();
     
+    @RequestMapping("/triangle")
+    public Triangle triangle(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Triangle(counter.incrementAndGet(), String.format(template, name));
+    }
 }
